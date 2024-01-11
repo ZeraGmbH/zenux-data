53e8a0c509b39a61c6ef248163a4edeb23cfab5e
Anuja Mutalik <a.mutalik@zera.de>
2024-01-11 18:15:53 +0100

    updateReadMe.py: Parse SessionNamesMapping.json to get session display strings
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- updateReadMe.py
+++ updateReadMe.py
@@ -1,6 +1,27 @@
 import os
 import sys
+import json
 from mdutils.mdutils import MdUtils
+
+jsonFile = sys.argv[2]
+sessionNamesMap = json.load(open(jsonFile))
+mtmap = sessionNamesMap["mt310s2"]
+commap = sessionNamesMap["com5003"]
+mdSpecialSyntax = ["*", "**", ">", "- ", "# ", "## ", "### "]
+
+mtmap = {key.replace("json", "html"):value for key,value in mtmap.items()}
+commap = {key.replace("json", "html"):value for key,value in commap.items()}
+
+
+for session in mtmap :
+    for specialChar in mdSpecialSyntax :
+        escapeChar = "\\" + specialChar
+        mtmap[session] = mtmap[session].replace(specialChar, escapeChar)
+
+for session in commap :
+    for specialChar in mdSpecialSyntax :
+        escapeChar = "\\" + specialChar
+        commap[session] = commap[session].replace(specialChar, escapeChar)
 
 #Recreate from title to 'Previous versions'
 newReadMe = MdUtils(file_name='NEW_README.md',title='zenux-data')
@@ -11,17 +32,12 @@ newReadMe.new_header(level=2, title='SCPI Interface Descriptions', add_table_of_
 newReadMe.new_paragraph("Given below are links to the most recent versions of these documents:")
 
 newReadMe.new_header(level=3, title='MT310s2', add_table_of_contents="n")
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/mt310s2-meas-session.html', text='Default Session'))
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/mt310s2-emob-session.html', text='EMOB AC/DC Session'))
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/mt310s2-emob-session-ac.html', text='EMOB AC Session')) 
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/mt310s2-emob-session-dc.html', text='EMOB DC Session')) 
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/mt310s2-dc-session.html', text='DC: 4\*Voltage / 1\*Current Session'))
+for i in mtmap :
+    newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/' + i, text=mtmap[i] + ' session'))
 
 newReadMe.new_header(level=3, title='COM5003', add_table_of_contents="n")
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/com5003-meas-session.html', text='Default Session'))
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/com5003-ced-session.html', text='Changing energy direction Session'))
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/com5003-ref-session.html', text='Reference Session')) 
-newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/com5003-perphase-session.html', text='3 Systems / 2 Wires Session'))
+for i in commap :
+    newReadMe.new_line('- ' + newReadMe.new_inline_link(link='https://ZeraGmbH.github.io/zenux-data/scpi-documentation/' + i, text=commap[i] + ' session'))
 
 newReadMe.new_header(level=3, title='Previous versions', add_table_of_contents="n")
 
